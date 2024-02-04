db155eec083bed4697c1e07f0047a2f347e757c6
Mariano Alfonso <marianoalfonso@gmail.com>
2024-02-04 13:00:01 -0300

    previo clase after servicios

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
+  // // basicamente los subjects son notificadores de cuando ocurre algo
+  // // al setearla privada, cancelamos la posibilidad de ser seteada desde el exterior de la clase
+  // private loadingTriggered$ = new Subject<boolean>();
 
-  constructor() { }
-
-  // basicamente los subjects son notificadores de cuando ocurre algo
-  // al setearla privada, cancelamos la posibilidad de ser seteada desde el exterior de la clase
-  private loadingTriggered$ = new Subject<boolean>();
+  // a deferencia del behaviour, el behaviourSubject debe recibir un valor inicial
+  private loadingTriggered$ = new BehaviorSubject<boolean>(false);
 
 
   // para exponerla y poder cambiar el valor desde afuera de la clase
