4ec5f5b1fa624cbf343dccf667e1ea4a30a84ee6
marianoalfonso@gmail.com <marianoalfonso@gmail.com>
2024-02-07 23:05:35 -0300

    unit testing

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// este modulo core tiene la funcionalidad de almacenar
+// todos los ervicios y funciones que la app nececita para funcionars
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
